169d465071fcbdae7dc4fb7d80863b1efc7f6102
utripathy <upendra.tripathy@gmail.com>
2025-06-04 09:57:36 +0530

    Reaction Tester Project files

---
+++ script.js
@@ -0,0 +1,52 @@
+var startTime = new Date().getTime();
+
+//random color generator
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+//movable box
+function movableBox() {
+  var left;
+  var top;
+  var wh;
+
+  left = Math.random() * 300;
+  top = Math.random() * 300;
+  wh = Math.random() * 400 + 100;
+
+  document.getElementById('box').style.left = left + 'px';
+  document.getElementById('box').style.top = top + 'px';
+  document.getElementById('box').style.width = wh + 'px';
+  document.getElementById('box').style.height = wh + 'px';
+  document.getElementById('box').style.display = 'block';
+
+  // Change the box's color after the result is shown.
+  document.getElementById('box').style.backgroundColor = getRandomColor();
+
+  startTime = new Date().getTime();
+}
+
+movableBox();
+
+// Handles the logic for moving the box and calculating reaction time.
+document.getElementById('box').onclick = function () {
+  document.getElementById('box').style.display = 'none';
+  var endTime = new Date().getTime();
+  var timeTaken = (endTime - startTime) / 1000;
+
+  // Display the reaction time in seconds or minutes.
+  alert(timeTaken);
+
+  document.getElementById('box').style.display = 'block';
+
+  movableBox();
+};
+
+// Movable box each second
+setInterval(movableBox, 1000);
